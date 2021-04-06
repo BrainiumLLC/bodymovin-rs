7f251e0182f9945b14609d7f1b0a7366d2fd0030
Francesca Lovebloom <francesca@brainiumstudios.com>
2021-04-06 14:23:27 -0700

    Make more fields pub

--- src/helpers/transform.rs
+++ src/helpers/transform.rs
@@ -4,23 +4,23 @@ use serde::Deserialize;
 #[derive(Debug, Deserialize)]
 pub struct Transform {
     #[serde(rename = "a", default = "properties::EitherMultiDimensional::zero")]
-    anchor_point: properties::EitherMultiDimensional,
+    pub anchor_point: properties::EitherMultiDimensional,
     #[serde(rename = "p", default = "properties::EitherMultiDimensional::zero")]
-    position: properties::EitherMultiDimensional,
+    pub position: properties::EitherMultiDimensional,
     #[serde(rename = "s", default = "properties::EitherMultiDimensional::hundred")]
-    scale: properties::EitherMultiDimensional,
+    pub scale: properties::EitherMultiDimensional,
     #[serde(rename = "r", default = "properties::EitherValue::zero")]
-    rotation: properties::EitherValue,
+    pub rotation: properties::EitherValue,
     #[serde(rename = "o", default = "properties::EitherValue::hundred")]
-    opacity: properties::EitherValue,
+    pub opacity: properties::EitherValue,
     #[serde(rename = "px", default = "properties::EitherValue::zero")]
-    position_x: properties::EitherValue,
+    pub position_x: properties::EitherValue,
     #[serde(rename = "py", default = "properties::EitherValue::zero")]
-    position_y: properties::EitherValue,
+    pub position_y: properties::EitherValue,
     #[serde(rename = "pz", default = "properties::EitherValue::zero")]
-    position_z: properties::EitherValue,
+    pub position_z: properties::EitherValue,
     #[serde(rename = "sk", default = "properties::EitherValue::zero")]
-    skew: properties::EitherValue,
+    pub skew: properties::EitherValue,
     #[serde(rename = "sa", default = "properties::EitherValue::zero")]
-    skew_axis: properties::EitherValue,
+    pub skew_axis: properties::EitherValue,
 }
